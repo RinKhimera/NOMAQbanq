e5cd8b5cac1b0c9af9ade42615232ddb57362b31
Daryl <borismbakop611@gmail.com>
2025-08-12 04:28:34 +0100

    Remove column toggle functionality from DataTable component for a cleaner UI

--- components/ui/data-table.tsx
+++ components/ui/data-table.tsx
@@ -136,35 +136,6 @@ export function DataTable<TData, TValue>({
             className="max-w-sm"
           />
         )}
-
-        {showColumnToggle && (
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="outline" className="ml-auto">
-                Colonnes <ChevronDown className="ml-2 h-4 w-4" />
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              {table
-                .getAllColumns()
-                .filter((column) => column.getCanHide())
-                .map((column) => {
-                  return (
-                    <DropdownMenuCheckboxItem
-                      key={column.id}
-                      className="capitalize"
-                      checked={column.getIsVisible()}
-                      onCheckedChange={(value) =>
-                        column.toggleVisibility(!!value)
-                      }
-                    >
-                      {column.id}
-                    </DropdownMenuCheckboxItem>
-                  )
-                })}
-            </DropdownMenuContent>
-          </DropdownMenu>
-        )}
       </div>
 
       <div className="rounded-md border">
